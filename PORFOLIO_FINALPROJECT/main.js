df98a969b996b834b91ec925d909fc1c6748030d
DESKTOP-GV16N45\Alumno <informatica@esne.es>
2025-04-22 15:52:11 +0200

    AAAAA day a lot

--- PORFOLIO_FINALPROJECT/main.js
+++ PORFOLIO_FINALPROJECT/main.js
@@ -2,3 +2,61 @@ window.addEventListener("scroll", function(){
     const nav = document.querySelector("nav");
     nav.classList.toggle("sticky", window.scrollY > 0);
 });
+
+const sentences = [
+    "Nice to meet you!",
+    "Welcome to my Porfolio",
+    "Spreadsheet enthusiast",
+    "Sure you can stand a little search, right...?"
+];
+
+let currentSentenceIndex = Math.floor(Math.random() * sentences.length);
+let currentSentence = sentences[currentSentenceIndex];
+const animatedWelcome = document.querySelector('.animated-welcome h3'); 
+let typingSpeed = 100; 
+let erasingSpeed = 50; 
+let pauseDuration = 1500; 
+let currentCharIndex = 0; 
+let isErasing = false;
+
+function typeText() {
+    let typingInterval = setInterval(() => {
+        animatedWelcome.textContent += currentSentence[currentCharIndex];
+        currentCharIndex++;
+    
+        if (currentCharIndex === currentSentence.length) {
+            clearInterval(typingInterval);
+    
+            setTimeout(eraseText, pauseDuration);
+        }
+    }, typingSpeed);
+
+
+}
+
+
+function eraseText() {
+    if (!isErasing) {
+        isErasing = true; // Mark that we are in the process of erasing
+
+        let erasingInterval = setInterval(() => {
+            animatedWelcome.textContent = animatedWelcome.textContent.slice(0, currentCharIndex - 1);
+            currentCharIndex--;
+        
+            if (currentCharIndex === 0) {  // Once the text is erased
+                clearInterval(erasingInterval);
+
+                setTimeout(() => {
+                    currentSentenceIndex = Math.floor(Math.random() * sentences.length); // Pick a new random sentence
+                    currentSentence = sentences[currentSentenceIndex]; // Update the sentence
+                    isErasing = false; // Reset erasing flag
+                    currentCharIndex = 0; // Reset char index for the next sentence
+                    setTimeout(typeText, pauseDuration); // Start typing the new sentence after a delay
+                }, pauseDuration);
+            }
+        }, erasingSpeed);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", typeText);
+
